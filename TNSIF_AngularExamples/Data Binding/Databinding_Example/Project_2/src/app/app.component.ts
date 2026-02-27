270cf4c3b6a7cdd204c99e5504d39847ffe56ca7
Harini DJ <115467935+Harinidj@users.noreply.github.com>
2026-02-27 18:07:34 +0530

    Commit 1

---
+++ TNSIF_AngularExamples/Data Binding/Databinding_Example/Project_2/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Types of Data Binding';
+
+
+  //interpolation - one way data binding
+  public name:String = 'KIT & DSATM College - Bangalore';
+
+
+  //property data binding
+
+
+  public imgUrl:String = 'Flower.jpg';
+
+
+  //Event Binding
+
+public message:String = '';
+
+
+public location = 'Bangalore, Karnataka';
+
+
+public isvisible:boolean = false;
+
+
+public course:String[] = ["Java", "Python", "CSS", "HTML", "Angular"];
+
+
+onClick():void{
+  this.message = "Welcome to Learn Anuglar";
+}
+
+toggleButton():void{
+
+this.isvisible=!this.isvisible;
+
+}
+
+}
